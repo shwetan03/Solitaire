a6342d3d42270da7c5ee961a755ba2d6f8a622e5
shwetan03 <43916633+shwetan03@users.noreply.github.com>
2022-05-28 10:01:20 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,12 +8,32 @@ public class Main {
 }
 
 class Card {
-    private String number;
     private String suit;
+    private String number;
 
-    public Card(String number, String suit)
+    public Card(String suit, String number)
     {
-        this.number = number;
         this.suit = suit;
+        this.number = number;
+    }
+}
+
+class SolitaireGame {
+    public static ArrayList<Card> makeDeck()
+    {
+        String[] suits = {"heart", "diamond", "spade", "clubs"};
+        String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"};
+        
+        ArrayList<Card> deck = new ArrayList<>();
+
+        for (int i = 0; i < suits.length; i++)
+        {
+            for (int j = 0; j < numbers.length; j++)
+            {
+                deck.add(new Card(suits[i], numbers[j]));
+            }
+        }
+        
+        return deck;
     }
 }
