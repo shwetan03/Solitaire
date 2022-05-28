442f6678cd74a74d89ca48bfc99c64651c652b82
shwetan03 <43916633+shwetan03@users.noreply.github.com>
2022-05-28 10:51:02 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -29,11 +29,18 @@ class Card {
 }
 
 class SolitaireGame {
+    private ArrayList<Card> deck;
+
+    public SolitaireGame()
+    {
+        this.deck = makeDeck();
+    }
+
     public static ArrayList<Card> makeDeck()
     {
-        String[] suits = {"heart", "diamond", "spade", "clubs"};
-        String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"};
-        
+        String[] suits = {"hearts", "diamonds", "spades", "clubs"};
+        String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
         ArrayList<Card> deck = new ArrayList<>();
 
         for (int i = 0; i < suits.length; i++)
@@ -43,7 +50,14 @@ class SolitaireGame {
                 deck.add(new Card(suits[i], numbers[j]));
             }
         }
-        
+
+        Collections.shuffle(deck);
+
+        return deck;
+    }
+
+    public ArrayList<Card> getDeck()
+    {
         return deck;
     }
 }
