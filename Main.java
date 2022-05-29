82b020b9d14cd240f241da14e064248913cf832e
shwetan03 <43916633+shwetan03@users.noreply.github.com>
2022-05-28 21:12:55 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -62,3 +62,24 @@ class SolitaireGame {
         return deck;
     }
 }
+
+class CardPile {
+    private ArrayList<Card> deck;
+    private Stack<Card> cardStack;
+
+    public CardPile(ArrayList<Card> deck)
+    {
+        this.deck = deck;
+        this.cardStack = deal();
+    }
+
+    public Stack<Card> deal() {
+        Stack<Card> cardStack = new Stack<>();
+
+        for (int i = 0; i < (52-28); i++)
+        {
+            cardStack.push(deck.get(i));
+        }
+        return cardStack;
+    }
+}
