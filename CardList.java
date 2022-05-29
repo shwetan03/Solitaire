a08d3c49d01ff1f6ba4b0bf0dc88fcb82361ecf7
shwetan03 <43916633+shwetan03@users.noreply.github.com>
2022-05-29 14:45:33 -0700

    Update CardList.java

--- CardList.java
+++ CardList.java
@@ -2,4 +2,16 @@ package com.company;
 
 public class CardList
 {
+    private CardListNode head;
+    private CardListNode tail;
+
+    public CardListNode getHead()
+    {
+        return head;
+    }
+
+    public CardListNode getTail()
+    {
+        return tail;
+    }
 }
