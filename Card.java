7b24d695680b3f2d6ae6aeda070b848b36867168
shwetan03 <43916633+shwetan03@users.noreply.github.com>
2022-05-29 14:52:18 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -3,11 +3,25 @@ package com.company;
 public class Card {
     private String suit;
     private String number;
+    private int value;
 
     public Card(String suit, String number)
     {
         this.suit = suit;
         this.number = number;
+        try {
+            this.value = Integer.parseInt(this.number);
+        } catch (Exception e) {
+            if (this.number.equals("A")) {
+                this.value = 1;
+            } else if (this.number.equals("J")) {
+                this.value = 11;
+            } else if (this.number.equals("Q")) {
+                this.value = 12;
+            } else {
+                this.value = 13;
+            }
+        }
     }
 
     public String getSuit()
@@ -18,5 +32,10 @@ public class Card {
     public String getNumber()
     {
         return number;
+    }
+
+    public int getValue()
+    {
+        return value;
     }
 }
